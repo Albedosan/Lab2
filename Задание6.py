4677402f0f28b7a43a1bfaa82c2968448b205845
Albedosan <92673301+Albedosan@users.noreply.github.com>
2021-11-01 12:07:22 +0300

    Update Задание6.py

--- Задание6.py
+++ Задание6.py
@@ -1,3 +1,10 @@
-n=01
-print(n)
-
+print('Введите значение N от 1 до 100')
+N=int(input())
+if (N>=5) and(N<21):
+    print(N,'Лет')
+if(N%10==1) and((N<5) or(N>20)):
+    print(N,'Год')
+if(N%10>1) and(N%10<5) and((N<5) or(N>20)):
+    print(N,'Года')
+if((N%10==0) or(N%10>5)) and((N<5) or(N>20)):
+    print(N,'Лет')
